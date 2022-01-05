5e66baf45c3cee019b23ce9f7f1dd0437a5f5cb4
pfrueh1 <fruehpaul@gmail.com>
2022-01-05 11:27:34 -0500

    hooks for filtering, document title

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
+
+function Gallery(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+      } = props;
+
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <div>
+        <img
+          src={photo}
+          alt="Commercial Example"
+          className="img-thumbnail mx-1"
+        />
+      </div>
+    </section>
+  );
+}
+export default Gallery;
